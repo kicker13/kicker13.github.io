752a740060ad74efee0c4c6268f4ff72707a1395
kicker13 <akshaykumar2835@gmail.com>
2022-06-15 19:25:03 +0530

    updated

--- web.js
+++ web.js
@@ -1,3 +1,9 @@
 
 
 
+const menubutton = document.querySelector(".menubtn")
+const navlinks = document.querySelector(".nav-links")
+
+menubutton.addEventListener("click", () => {
+    navlinks.classList.toggle("mobile-menu")
+})
